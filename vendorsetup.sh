2b371b801597ae1a1a1a8a0cddd63748113048ba
sub77 <sub77@ymail.com>
2015-10-03 06:53:34 -0400

    update

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,5 @@
 add_lunch_combo cm_matisse-eng
 add_lunch_combo cm_matisse-userdebug
-sh device/samsung/matisse-common/patches/apply.sh
-
+sh device/samsung/matisse/patches/apply.sh
+cp device/samsung/matisse/matisse.dependencies vendor/pac/dependencies
+cp device/samsung/matisse/pac_matisse.mk vendor/pac/products
